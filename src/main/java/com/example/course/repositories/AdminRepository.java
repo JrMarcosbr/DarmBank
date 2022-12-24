e328e54e7445fb3893cf7c91475963aa3e4ebb54
JrMarcosbr <42240044+JrMarcosbr@users.noreply.github.com>
2022-12-23 23:38:23 -0300

    update

--- src/main/java/com/example/course/repositories/AdminRepository.java
+++ src/main/java/com/example/course/repositories/AdminRepository.java
@@ -4,7 +4,6 @@ package com.example.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.course.entities.Admin;
-import com.example.course.entities.User;
 
 public interface AdminRepository extends JpaRepository<Admin, Long>{
 }
